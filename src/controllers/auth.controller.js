b54163fed55bbe0d5dc7af47297e06f5733bd8cb
Parinya Phapha <parinyaphapha@Parinyas-MacBook-Pro.local>
2021-04-19 19:41:16 +0700

    first commit

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -61,7 +61,7 @@ exports.signinFacebook = async (req, res) => {
       userModel.insertMany({
           username: req.body.first_name+' '+req.body.last_name,
           facebookId: req.body.id,
-          email: req.body.email
+          email: email===null? null : req.body.email
       }).then((user) => {
           var user = user[0];
           var token = tokenHandler.createToken(user._id, user.tokenVersion);
