550a264b1ad0f9d15251529ce3531c2a066824e7
Parinya Phapha <parinyaphapha@Parinyas-MacBook-Pro.local>
2021-04-19 21:26:04 +0700

    first commit

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -61,14 +61,13 @@ exports.signinFacebook = async (req, res) => {
       await userModel.insertMany({
           username: req.body.first_name+' '+req.body.last_name,
           facebookId: req.body.id,
-          email: req.body.email
+          email: 'bb.b@gmail.com'
       }).then((user) => {
           var user = user[0];
           var token = tokenHandler.createToken(user._id, user.tokenVersion);
           tokenHandler.sendToken(res, token);
  
       }).catch(error => {
-        console.log('22222222222222');
           res.status(400).json({ message: 'Insert not found!'});
       });
   }
