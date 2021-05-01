07534dc0ae89570c8970db8920c504b05de2777e
Parinya Phapha <parinyaphapha@Parinyas-MacBook-Pro.local>
2021-05-01 09:24:12 +0700

    first commit

--- src/models/basket.model.js
+++ src/models/basket.model.js
@@ -15,9 +15,3 @@ const basketSchema = mongoose.Schema({
 });
 
 module.exports = mongoose.model('basket', basketSchema);
-
-
-git add .
-git commit -m "first commit"  
-git branch -M main
-git push -u origin main
